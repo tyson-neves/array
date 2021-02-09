9910a2859d0e014eea95825e1e132ffefe314406
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-02-09 19:12:42 -0300

    Update basico_1.js

--- basico_1.js
+++ basico_1.js
@@ -6,6 +6,7 @@ const notas = [7.8, 6.7, 10, 9.5, 7.8, 5.8];
 console.log(notas);
 console.log(notas[3]);
 console.log(notas[5]);
+console.log(notas[2]);
 console.log(notas[600]); // undefined pq não foi passado esse array
 
 notas[4] = 8.7; // altera o valor do array 4, mesmo sendo uma const, ja que o conteúdo do array é independente
