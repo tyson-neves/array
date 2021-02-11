38ef8d3ec79f5e01e4771eef9b6f4c2fd1244856
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-02-11 20:19:37 -0300

    Create for_1.js

---
+++ for_1.js
@@ -0,0 +1,10 @@
+const numeros = [1, 2, 3, 4, 5, 6,556, 45, 223];
+
+for(let i = 0; i < numeros.length; i++) {
+    console.log(i, numeros[i]);
+}
+
+
+for(let i = 0; i < numeros.length; i+= 2) {
+    console.log(i, numeros[i]);
+}
